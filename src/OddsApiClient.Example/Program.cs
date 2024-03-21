2881c36e9a834f31a7d6e9fab598d2154c91b40d
bstoker-ir <bens@informativeresearch.com>
2024-03-21 09:05:28 -0400

    Added example project

---
+++ src/OddsApiClient.Example/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using OddsApiClient.Requests;
+
+namespace OddsApiClient.Example;
+
+public class Program
+{
+  static async Task Main(string[] args)
+  {
+    var baseUrl = "https://api.the-odds-api.com";
+    var apiKey = ""; // Add your free API Key here
+    var client = new OddsClient(baseUrl, apiKey);
+    
+    // Retrieve Sports
+    var request = new RetrieveSportsRequest
+    {
+      All = true
+    };
+
+    var response = await client.RetrieveSportsAsync(request);
+
+    Console.WriteLine(JsonSerializer.Serialize(response));
+  }
+}
